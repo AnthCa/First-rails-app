19e9e8eb612f7b0e72c8d8c2b8565372ec00bbd3
Anthony Canales <anthonycca8@gmail.com>
2016-12-27 22:11:03 -0300

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :publications
   get 'main/index'
 
-  root 'main#index'
+  root 'publications#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
